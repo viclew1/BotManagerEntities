2da00dc7b8ee10e97c8517bc90032d106142f12f
Victor Lewon <viclew@gmail.com>
2020-03-12 00:23:14 +0100

    Save commit

--- src/main/java/fr/lewon/bot/manager/entities/BotInfoDTO.kt
+++ src/main/java/fr/lewon/bot/manager/entities/BotInfoDTO.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class BotInfoDTO(
         @field:JsonProperty var id: Long = -1,
+        @field:JsonProperty("game_id") var gameId: Long = -1,
         @field:JsonProperty var login: String = "",
         @field:JsonProperty var state: String = "",
         @field:JsonProperty("available_transitions") var availableTransitions: List<String> = emptyList()
