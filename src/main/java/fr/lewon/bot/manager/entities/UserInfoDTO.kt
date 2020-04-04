df7d921166c6a4c63c4964c7ed0b6c317d139725
Victor Lewon <viclew@gmail.com>
2020-04-04 16:35:51 +0200

    Correcting json property value

--- src/main/java/fr/lewon/bot/manager/entities/UserInfoDTO.kt
+++ src/main/java/fr/lewon/bot/manager/entities/UserInfoDTO.kt
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UserInfoDTO(
         @field:JsonProperty var login: String = "",
-        @field:JsonProperty var loginProperties: Map<String, String> = emptyMap(),
+        @field:JsonProperty("login_properties") var loginProperties: Map<String, String> = emptyMap(),
         @field:JsonProperty var params: Map<String, String?> = emptyMap()
 )
